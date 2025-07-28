856a082823617ecf5b85ed6d57a389ad9facf019
shilpb20 <shilpb20@gmail.com>
2025-07-28 18:56:15 +1000

    test: add test for POST /api/todo-list endpoint

---
+++ TodoList.TestDataBuilder/TodoItemCreateDtoBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.TestDataBuilder
+{
+    public class TodoItemCreateDtoBuilder
+    {
+        private string _description = "Default description";
+        private string _title = "Default title";
+
+        private DateTime _dueAt = DateTime.UtcNow.AddDays(1);
+
+        public TodoItemCreateDtoBuilder WithDescription(string description)
+        {
+            _description = description;
+            return this;
+        }
+
+        public TodoItemCreateDtoBuilder WithDetails(string details)
+        {
+            _title = details;
+            return this;
+        }
+
+        public TodoItemCreateDtoBuilder WithDueDate(DateTime dueDate)
+        {
+            _dueAt = dueDate;
+            return this;
+        }
+
+        public TodoItemCreateDto Build()
+        {
+            return new TodoItemCreateDto
+            {
+                Description = _description,
+                Title = _title,
+                DueAt = _dueAt
+            };
+        }
+    }
+}
--- src/TodoList.Application/DTOs/TodoItemDto.cs
+++ src/TodoList.Application/DTOs/TodoItemDto.cs
@@ -1,6 +1,19 @@
-﻿namespace TodoList.Application.DTOs
+﻿using TodoList.Domain;
+
+namespace TodoList.Application.DTOs
 {
     public class TodoItemDto
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string? Description { get; set; } = string.Empty;
+
+        public DateTime? DueAt { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public TodoStatus Status { get; set; }
     }
 }
