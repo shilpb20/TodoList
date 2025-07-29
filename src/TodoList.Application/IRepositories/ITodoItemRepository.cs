f55d562e1f32f4c54ad5ac3fa738f1f6cfaf3138
shilpb20 <shilpb20@gmail.com>
2025-07-29 18:16:54 +1000

    feat: add delete and get all methods to TODO repository.

--- src/TodoList.Application/IRepositories/ITodoItemRepository.cs
+++ src/TodoList.Application/IRepositories/ITodoItemRepository.cs
@@ -5,5 +5,7 @@ namespace TodoList.Application.IRepositories
     public interface ITodoItemRepository
     {
         Task<TodoItem> AddAsync(TodoItem todoItem);
+        Task<TodoItem?> DeleteAsync(Guid id);
+        Task<IEnumerable<TodoItem>> GetAllAsync();
     }
 }
