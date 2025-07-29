9854ce51734a22180c24bdaa3fbecf2278bf84b7
shilpb20 <shilpb20@gmail.com>
2025-07-29 18:21:24 +1000

    feat: update mapping and add tests.

--- src/TodoList.Infrastructure/Mapper/TodoItemProfile.cs
+++ src/TodoList.Infrastructure/Mapper/TodoItemProfile.cs
@@ -1,12 +1,7 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TodoList.Application.DTOs;
 using TodoList.Domain.Entities;
-using TodoList.TestDataBuilder;
+using TodoList.Domain.Enums;
 
 namespace TodoList.Infrastructure.Mapper
 {
@@ -14,10 +9,24 @@ namespace TodoList.Infrastructure.Mapper
     {
         public TodoItemProfile()
         {
-            CreateMap<TodoItem, TodoItemCreateDto>();
+            CreateMap<TodoItemCreateDto, TodoItem>()
+                .ConstructUsing(dto => new TodoItem(dto.Title, dto.Description))
+                .ForMember(destination => destination.Id, opt => opt.Ignore())
+                .ForMember(destination => destination.CreatedAt, opt => opt.Ignore())
+                .ForMember(destination => destination.Status, opt => opt.Ignore());
 
             CreateMap<TodoItem, TodoItemDto>()
                 .ForMember(destination => destination.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+
+            CreateMap<TodoItemDto, TodoItem>()
+                        .ForMember(destination => destination.Status, opt => opt.MapFrom(src => ParseStatus(src.Status)));
+        }
+
+        private static TodoStatus ParseStatus(string status)
+        {
+            return Enum.TryParse<TodoStatus>(status, out var parsedStatus)
+                ? parsedStatus
+                : TodoStatus.Pending;
         }
     }
 }
