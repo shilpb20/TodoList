4b5dbdfea5fcb0b4c3f1581bbeeb20e50421ffdb
shilpb20 <shilpb20@gmail.com>
2025-07-30 15:44:17 +1000

    feat: Implement view todo items

--- client/src/app/features/todo/todo.ts
+++ client/src/app/features/todo/todo.ts
@@ -1,39 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TodoService, TodoItem } from '../../core/services/todo.service';
 
 @Component({
   standalone: true,
   selector: 'app-todo',
   templateUrl: './todo.html',
   styleUrls: ['./todo.css'],
-  imports: [FormsModule, CommonModule],
+imports: [FormsModule, CommonModule],
 })
-export class Todo {
+
+export class Todo implements OnInit {
   title = '';
   description = '';
 
-  tasks = [
-    {
-      id: 1,
-      title: 'Buy groceries',
-      description: 'Milk, Bread, Eggs',
-      showDescription: false,
-    },
-    {
-      id: 2,
-      title: 'Call plumber',
-      description: '',
-      showDescription: false,
-    },
-  ];
+  tasks: (TodoItem & { showDescription: boolean })[] = [];
+
+    constructor(private todoService: TodoService) {
+        console.log('Todo component constructed');
+    } 
+
+  ngOnInit(): void {
+    this.todoService.getAll().subscribe({
+      next: (todos) => {
+        this.tasks = todos.map((todo) => ({ ...todo, showDescription: false }));
+      },
+      error: (err) => {
+        console.error('Error fetching todos:', err);
+      },
+    });
+  }
 
   toggleDesc(index: number): void {
     this.tasks[index].showDescription = !this.tasks[index].showDescription;
   }
 
   onAdd(): void {
-    // For now, just log input
     console.log('Submitted:', this.title, this.description);
     this.title = '';
     this.description = '';
