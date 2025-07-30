1bd73d8aaf983f825bb8af2e1abe4c62e9bec51d
shilpb20 <shilpb20@gmail.com>
2025-07-30 10:20:11 +1000

    feat: add static ui for todo.

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './features/todo/todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Todo],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('ng-temp');
--- client/src/app/features/todo/todo.ts
+++ client/src/app/features/todo/todo.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-todo',
-  imports: [],
   templateUrl: './todo.html',
-  styleUrl: './todo.css'
+  styleUrls: ['./todo.css'],
+  imports: [FormsModule],
 })
-export class Todo {
 
+export class Todo {
+  title: string = '';
+  description: string = '';
+
+  onAdd() {
+    console.log('Add clicked:', this.title, this.description);
+    // You can later add code here to submit to backend
+
+    // Clear inputs after submit
+    this.title = '';
+    this.description = '';
+  }
 }
